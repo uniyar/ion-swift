5dcbee2cdfa7554975cbc68633814243062b1179
hellc <ivanmanov@live.com>
2020-01-04 23:22:40 +0300

    Base framework
    Donor core
    Deprecations fix
    Catalyst env

---
+++ Framework/ion-swift/ion_swift.h
@@ -0,0 +1,19 @@
+//
+//  ion_swift.h
+//  ion-swift
+//
+//  Created by Ivan Manov on 04.01.2020.
+//  Copyright © 2020 kxpone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ion_swift.
+FOUNDATION_EXPORT double ion_swiftVersionNumber;
+
+//! Project version string for ion_swift.
+FOUNDATION_EXPORT const unsigned char ion_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ion_swift/PublicHeader.h>
+
+
